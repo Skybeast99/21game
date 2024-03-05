3f7560a3ce9a2c7290826f4d2c319264042be3ae
Skybeast99 <145792205+Skybeast99@users.noreply.github.com>
2024-03-05 19:44:08 +0300

    Add files via upload

--- 21_game.py
+++ 21_game.py
@@ -1,8 +1,9 @@
 from random import shuffle
+from os import system
 
-suits = ('пик', 'крести', 'буби', 'черви')
-
-cards = {
+def get_deck() -> list[dict]:
+    suits = ('пик', 'крести', 'буби', 'черви')
+    cards = {
     '2': 2,
     '3': 3,
     '4': 4,
@@ -16,46 +17,75 @@ cards = {
     'дама': 10,
     'король': 10,
     'туз': 11
+    }
 
-}
+    deck = []
+
+    for suit in suits:
+        for item in cards:
+            card = {
+                'цена': cards[item],
+                'масть': suit,
+                'название': item,
+            }
+            deck.append(card)
+    return deck
 
 
-deck = []
+def get_players() -> list[dict]:
+    '''возращает список играков'''
+    player_1 = {
+        'имя': 'вася',
+        'карты': [],
+        'человек': True,
+        'сумма': 0
+    }
 
-for suit in suits:
-    for item in cards:
-        card = {
-            'цена' : cards[item],
-            'масть' : suit,
-            'название' : item,
-        }
-        deck.append(card)
+    player_2 = {
+        'имя': 'ася',
+        'карты': [],
+        'человек': True,
+        'сумма': 0
+    }
+    return [player_1, player_2]
 
+
+def deal_cards(num: int) -> None:
+    '''раздает каждому игроку карты'''
+    for player in players:
+        for i in range(num):
+            player['карты'].append(deck.pop())
+
+
+def show_cards() -> None:
+    for card in player['карты']:
+        print(card['название'], card['масть'])
+
+
+deck = get_deck()
 shuffle(deck)
+players = get_players()
+deal_cards(2)
 
-player_1 = {
-    'имя': 'вася',
-    'карты': [],
-    'человек': True
-}
 
-player_2 = {
-    'имя': 'вася',
-    'карты': [],
-    'человек': True
-}
-
-players = (player_1, player_2)
+if players[0]['карты'][1] in deck:
+    print('!!!!!')
 
 for player in players:
-    for i in range(2):
-        player['карты'].append(deck.pop())
+    while True:
+        system('cls')
+        show_cards()
+        player_option = input('взять карту из колоды (y/n)? ')
+        if player_option.lower() == 'y':
+            player['карты'].append(deck.pop())   
+        elif player_option.lower() == 'n':
+            break
+        else:
+            print('у вас опечатка')
 
 for player in players:
-    print(player['карты'])
+    for card in player['карты']:
+        player['сумма'] += card['цена']
+    print(player['имя'], player['сумма'])
 
-print('число карт в колоде: ', len(deck))
-
-for card in deck:
-    print(card)
-    print('-' * 50)
+print()
