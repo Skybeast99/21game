eb52a972f0e01657359bcb2588e48fbef26c7802
Skybeast99 <145792205+Skybeast99@users.noreply.github.com>
2024-03-26 19:06:51 +0300

    Add files via upload

--- pipyao.py
+++ pipyao.py
@@ -1,11 +1,32 @@
-def determine_section(x1, x2, y1, y2):
+def determine_section(x1, x2, y1, y2) -> str:
     x = set()
     y = set()
-    for i in range(x1, x2, 1):
-        x.add(i)
-    for m in range(y1, y2, 1):
-        y.add(m)
-    y.intersection(x)
-    return
 
-print(determine_section(1, 10, 5, 15))
+    if x1 < x2:
+        for i in range(x1, x2 + 1, 1):
+            x.add(i)   
+    elif x1 > x2:
+        for i in range(x1, x2 + 1, -1):
+            x.add(i)
+    else:
+        x.add(x1)
+    
+
+    if y1 < y2:
+        for m in range(y1, y2 + 1, 1):
+            y.add(m)
+    elif y1 > y2:    
+        for m in range(y1, y2 + 1, -1):
+            y.add(m)
+    else:
+        y.add(y2)
+
+
+    if y.intersection(x):
+        return 'да'
+    else:
+        return 'нет'
+    
+
+print(determine_section(7, 9, 10, 15))
+
